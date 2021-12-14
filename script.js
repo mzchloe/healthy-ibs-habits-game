e32478ef2ac2b48715f0ca408efe86e0b8647afc
Jeanet <linett@gmail.com>
2021-12-14 23:04:47 +0100

    Add files via upload
    Could not commit via terminal, added the files manually instead

---
+++ script.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('my-canvas');
+const ctx = canvas.getContext("2d");
+
+
+//background image - static
+const bgImg = new Image();
+bgImg.src = "./images/bg.jpeg";
+
+ctx.drawImage(bgImg,0, 0);
+
+//1. Create player 
+
+ctx.beginPath();
+ctx.fillRect(20, 20, 150, 150);
+ctx.fillStyle = "black";
+ctx.closePath();
+
+
+    
+//1.1 Player object: square (replace with image later)
+
+
+//1.2 Create move() function so the player can make movements
+
+
+//2.1 Create 1 falling object
+//2.2 Falling object: square (replace with images later)
+//2.3 Create a falling function, which is a scrolling function but vertically from X = 0
